1e9925d25a1d3b57a21b481b1d56a8767bc682e4
Avantii9 <105850924+Avantii9@users.noreply.github.com>
2022-05-25 15:34:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-streamlit.title('My Parents new healthy diner')streamlit.header('Breakfast Favorites')
+streamlit.title('My Parents new healthy diner')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
