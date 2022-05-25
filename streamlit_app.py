cca5137e63ad572a7516421ecf425dda8cc17198
Avantii9 <105850924+Avantii9@users.noreply.github.com>
2022-05-25 13:56:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-
+streamlit.stop()
 import snowflake.connector
 #  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #  my_cur = my_cnx.cursor()
@@ -70,20 +70,20 @@ my_cur = my_cnx.cursor()
 
 my_cur.execute("select * from fruit_load_list")
 
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 
 streamlit.text("The fruit load list contains:")
 
 streamlit.text(my_data_row)
               
- my_data_row= my_cur.fetchone()
- streamlit.header("The fruit load list contains:")
- streamlit.dataframe(my_data_row)
+#  my_data_row= my_cur.fetchone()
+#  streamlit.header("The fruit load list contains:")
+#  streamlit.dataframe(my_data_row)
 
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
- streamlit.dataframe(my_data_row)
+# my_cur.execute("select * from fruit_load_list")
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_row)
                   
                   
                   
